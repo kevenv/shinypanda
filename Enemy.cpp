1c2ace002f4534555cc58401de4f4a82f2710a08
Vincent Girard <vin100_jrare@hotmail.com>
2013-06-11 17:06:48 -0400

    Added enemy class.

---
+++ Enemy.cpp
@@ -0,0 +1,27 @@
+/**
+    @file Enemy.cpp
+    @author Vincent Girard <vin100_jrare@hotmail.com>
+    @version 1.0
+
+    @section LICENSE
+
+
+
+    @section DESCRIPTION
+
+    This file is the source of a class representing an enemy character.
+*/
+
+#include "Enemy.h"
+#include "Character.h"
+
+namespace spe
+{
+
+Enemy::Enemy(const char* name, const char* fileSprite, const char* filePosition, const int filePositionVersion, int x, int y)
+                                : Character(name, fileSprite, filePosition, filePositionVersion, x, y)
+{
+
+}
+
+}
---
+++ Enemy.h
@@ -0,0 +1,45 @@
+/**
+    @file Enemy.h
+    @author Vincent Girard <vin100_jrare@hotmail.com>
+    @version 1.0
+
+    @section LICENSE
+
+
+
+    @section DESCRIPTION
+
+    This file is the header of a class representing an enemy character.
+*/
+#ifndef SPE_ENEMY_H
+#define SPE_ENEMY_H
+
+#include "Character.h"
+
+namespace spe
+{
+
+/**
+    @class Ennemy Ennemy.h "Ennemy.h"
+    @brief An enemy.
+
+    This is an abstract class representing an enemy. It inherites of Character.
+*/
+class Enemy : public Character
+{
+public:
+    /**
+    Main constructor for the class.
+
+    @param[in] name Name of the ennemy.
+    @param[in] fileSprite Path of the file containing the sprites.
+    @param[in] filePosition Path of the file containing the infos of the sprites.
+    @param[in] filePositionVersion Last version of the positionf file.
+    @param[in] x,y Initial position of the character.
+    */
+    Enemy(const char* name, const char* fileSprite, const char* filePosition, const int filePositionVersion, int x = 0, int y = 0);
+};
+
+}
+
+#endif
